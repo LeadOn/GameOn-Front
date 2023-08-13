f8c99cfe5a37090b7ebbf89e417ea51ac60861d0
Valentin Virot <virot.valentin@gmail.com>
2023-08-13 22:34:29 +0200

    Added Team Logo Management

---
+++ src/app/services/yufoot-fifateam.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { FifaTeam } from "../classes/FifaTeam";
+
+@Injectable({
+  providedIn: "root",
+})
+export class YuFootFifaTeamService {
+  constructor(private client: HttpClient) {}
+
+  getAll(): Observable<FifaTeam[]> {
+    return this.client.get<FifaTeam[]>(
+      environment.yuFootApiUrl + "/fifateam/all"
+    );
+  }
+}
