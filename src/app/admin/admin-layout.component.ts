0c755f26836a2dac94d3377581ec5d5151d557af
Valentin Virot <virot.valentin@gmail.com>
2023-08-20 17:35:31 +0200

    Added basis for admin page.

--- src/app/admin/admin-layout.component.ts
+++ src/app/admin/admin-layout.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { initFlowbite } from "flowbite";
+import { environment } from "src/environments/environment";
 
 @Component({
-  selector: 'app-admin-layout',
-  templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss']
+  selector: "app-admin-layout",
+  templateUrl: "./admin-layout.component.html",
+  styleUrls: ["./admin-layout.component.scss"],
 })
-export class AdminLayoutComponent {
+export class AdminLayoutComponent implements OnInit {
+  ngOnInit(): void {
+    initFlowbite();
+  }
 
+  logout() {
+    window.location.replace(
+      environment.keycloak.url + "realms/yufoot/protocol/openid-connect/logout"
+    );
+  }
 }
--- src/app/admin/home/admin-home.component.ts
+++ src/app/admin/home/admin-home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Player } from "src/app/shared/classes/Player";
+import { YuFootPlayerService } from "src/app/shared/services/yufoot-player.service";
 
 @Component({
-  selector: 'app-admin-home',
-  templateUrl: './admin-home.component.html',
-  styleUrls: ['./admin-home.component.scss']
+  selector: "app-admin-home",
+  templateUrl: "./admin-home.component.html",
+  styleUrls: ["./admin-home.component.scss"],
 })
-export class AdminHomeComponent {
+export class AdminHomeComponent implements OnInit {
+  player: Player = new Player();
 
+  constructor(private playerService: YuFootPlayerService) {}
+
+  ngOnInit(): void {
+    this.playerService.getCurrent().subscribe(
+      (data) => {
+        this.player = data;
+      },
+      (err) => {
+        alert("Une erreur est survenue lors de la récupération du profile.");
+        console.error(err);
+      }
+    );
+  }
 }
