91151394613d209af073371ef8abbc98966761ac
Valentin Virot <virot.valentin@gmail.com>
2023-08-21 15:55:37 +0200

    Cleaned up some code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,25 @@
 import { APP_INITIALIZER, NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { environment } from "src/environments/environment";
+import { ClipboardModule } from "@angular/cdk/clipboard";
+
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { KeycloakAngularModule, KeycloakService } from "keycloak-angular";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { HomeComponent } from "./home/home.component";
-import { FooterComponent } from "./shared/components/footer/footer.component";
+import { FooterComponent } from "./shared/layouts/components/footer/footer.component";
 import { HomePlayersComponent } from "./home/components/players/home-players.component";
-import { PlayerCardComponent } from "./shared/components/player-card/player-card.component";
-import { HttpClientModule } from "@angular/common/http";
 import { GamesPlayedComponent } from "./home/components/games-played/games-played.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { PlayerDetailsComponent } from "./players/details/player-details.component";
-import { KeycloakAngularModule, KeycloakService } from "keycloak-angular";
-import { environment } from "src/environments/environment";
 import { MyDashboardComponent } from "./players/me/my-dashboard.component";
-import { PlayerDetailsCardComponent } from "./shared/components/player-details-card/player-details-card.component";
+import { PlayerDetailsCardComponent } from "./home/components/player-details-card/player-details-card.component";
 import { UpdatePlayerComponent } from "./players/me/components/update-player/update-player.component";
 import { CreateGameComponent } from "./games/create/create-game.component";
-import { ArrowBtnComponent } from "./shared/components/arrow-btn/arrow-btn.component";
-import { ClipboardModule } from "@angular/cdk/clipboard";
 import { GameDetailsComponent } from "./games/details/game-details.component";
-import { NotFoundComponent } from "./shared/components/not-found/not-found.component";
 import { AdminModule } from "./admin/admin.module";
 import { CommonLayoutComponent } from "./shared/layouts/common-layout.component";
 import { SharedModule } from "./shared/modules/shared.module";
@@ -48,16 +46,13 @@ function initializeKeycloak(keycloak: KeycloakService) {
     HomeComponent,
     FooterComponent,
     HomePlayersComponent,
-    PlayerCardComponent,
     GamesPlayedComponent,
     PlayerDetailsComponent,
     MyDashboardComponent,
     PlayerDetailsCardComponent,
     UpdatePlayerComponent,
     CreateGameComponent,
-    ArrowBtnComponent,
     GameDetailsComponent,
-    NotFoundComponent,
     CommonLayoutComponent,
   ],
   imports: [
--- src/app/shared/modules/shared.module.ts
+++ src/app/shared/modules/shared.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LoadingSpinnerComponent } from "../components/loading-spinner/loading-spinner.component";
 import { GamePlayedCardRowComponent } from "../components/game-played-card-row/game-played-card-row.component";
+import { PlayerCardComponent } from "../components/player-card/player-card.component";
+import { RouterModule } from "@angular/router";
+import { NotFoundComponent } from "../components/not-found/not-found.component";
+import { ArrowBtnComponent } from "../components/arrow-btn/arrow-btn.component";
 
 @NgModule({
-  declarations: [LoadingSpinnerComponent, GamePlayedCardRowComponent],
-  imports: [CommonModule],
-  exports: [LoadingSpinnerComponent, GamePlayedCardRowComponent],
+  declarations: [
+    LoadingSpinnerComponent,
+    GamePlayedCardRowComponent,
+    PlayerCardComponent,
+    NotFoundComponent,
+    ArrowBtnComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    LoadingSpinnerComponent,
+    GamePlayedCardRowComponent,
+    PlayerCardComponent,
+    NotFoundComponent,
+    ArrowBtnComponent,
+  ],
 })
 export class SharedModule {}
