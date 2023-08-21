14fc0578b796961dd1ebe0c9d527309d456c8072
Valentin Virot <virot.valentin@gmail.com>
2023-08-21 16:39:01 +0200

    Cleaned up some entities

--- src/app/shared/classes/GamePlayed.ts
+++ src/app/shared/classes/GamePlayed.ts
@@ -4,7 +4,7 @@ import { Team } from "./Team";
 export class GamePlayed {
   id: number = 0;
   playedOn: Date = new Date();
-  platform: string = "Unknown";
+  platform: string = "";
   platformId: number = 0;
   team1: Team = new Team();
   team2: Team = new Team();
--- src/app/shared/classes/Highlight.ts
+++ src/app/shared/classes/Highlight.ts
@@ -1,7 +1,7 @@
 export class Highlight {
   id: number = 0;
-  name: string = "Highlight #test";
-  CreatedById: number = 1;
-  GameId: number = 1;
-  externalUrl: string = "https://valentinvirot.fr";
+  name: string = "";
+  CreatedById: number = 0;
+  GameId: number = 0;
+  externalUrl: string = "";
 }
