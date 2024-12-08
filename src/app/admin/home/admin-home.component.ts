d72db6cfbc9e36573a4b07ad7bf32e4faaa1130d
Valentin Virot <virot.valentin@gmail.com>
2024-12-08 01:41:22 +0100

    Updated admin layout

--- src/app/admin/home/admin-home.component.ts
+++ src/app/admin/home/admin-home.component.ts
@@ -1,14 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { GameOnAdminService } from '../shared/services/gameon-admin.service';
 import { AdminDashboard } from '../shared/classes/AdminDashboard';
+import {
+  faCamera,
+  faComputer,
+  faRankingStar,
+  faSoccerBall,
+  faTrophy,
+  faUserCircle,
+  faVideo,
+} from '@fortawesome/free-solid-svg-icons';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-admin-home',
   templateUrl: './admin-home.component.html',
   styleUrls: ['./admin-home.component.scss'],
+  animations: [
+    trigger('inOutAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(200, style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate(200, style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class AdminHomeComponent implements OnInit {
   loading = true;
+  platformIcon = faComputer;
+  playerIcon = faUserCircle;
+  soccerIcon = faSoccerBall;
+  hightlightIcon = faVideo;
+  seasonIcon = faRankingStar;
+  tournamentIcon = faTrophy;
 
   stats: AdminDashboard = new AdminDashboard();
 
