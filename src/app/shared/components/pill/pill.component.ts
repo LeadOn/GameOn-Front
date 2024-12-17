b0a4f82fbd5cdb2912edbb7b2c0b3d2c5170b0b4
Valentin Virot <virot.valentin@gmail.com>
2024-12-17 13:15:38 +0100

    Updated home page to use standardized pill + added donation page

--- src/app/shared/components/pill/pill.component.ts
+++ src/app/shared/components/pill/pill.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {
   faExternalLink,
-  faInfoCircle,
   IconDefinition,
 } from '@fortawesome/free-solid-svg-icons';
 
@@ -10,7 +9,7 @@ import {
   templateUrl: './pill.component.html',
   styleUrl: './pill.component.scss',
 })
-export class PillComponent {
+export class PillComponent implements OnInit {
   @Input()
   title: string = 'Pill title';
 
@@ -26,5 +25,61 @@ export class PillComponent {
   @Input()
   iconColor = 'text-primary';
 
+  @Input()
+  bgStyle = 'primary';
+
+  @Input()
+  borderStyle?: string;
+
+  style = '';
+
   externalIcon = faExternalLink;
+
+  ngOnInit(): void {
+    this.style = 'bg-bgLight text-primary dark:bg-bgDark dark:text-primaryDark';
+
+    this.buildBgStyle(this.bgStyle);
+
+    if (this.borderStyle) {
+      this.buildBorderStyle(this.borderStyle);
+    }
+  }
+
+  buildBgStyle(bgStyle: string) {
+    switch (bgStyle) {
+      case 'primary':
+        this.style =
+          'bg-bgLight text-primary dark:bg-bgDark dark:text-primaryDark';
+        break;
+
+      case 'primary-dark':
+        this.style =
+          'bg-bgLightDarker text-primary dark:bg-bgDarkDarker dark:text-primaryDark';
+        break;
+
+      case 'secondary':
+        this.style = 'bg-secondary text-primaryDark';
+        break;
+
+      default:
+        this.style =
+          'bg-bgLight text-primary dark:bg-bgDark dark:text-primaryDark';
+        break;
+    }
+  }
+
+  buildBorderStyle(borderStyle: string) {
+    switch (borderStyle) {
+      case 'primary':
+        this.style += ' border border-primary dark:border-primaryDark';
+        break;
+
+      case 'red':
+        this.style += ' border border-frenchRed';
+        break;
+
+      default:
+        break;
+    }
+  }
 }
