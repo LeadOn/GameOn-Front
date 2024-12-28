0e4b2d91e28256877f8e13e83335ee7b1511fa7e
Valentin Virot <virot.valentin@gmail.com>
2024-12-28 19:52:39 +0100

    Updated LoL Game details UX

--- src/app/lol/games/details/lol-game-details.component.ts
+++ src/app/lol/games/details/lol-game-details.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { GameOnLoLService } from '../../../shared/services/leagueoflegends/gameon-lol.service';
 import { LoLGame } from '../../../shared/classes/LoLGame';
 import { ActivatedRoute } from '@angular/router';
+import {
+  faCalendar,
+  faExternalLink,
+  faSync,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-lol-game-details',
@@ -10,8 +15,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class LolGameDetailsComponent implements OnInit {
   gameId: any;
+
   game: LoLGame = new LoLGame();
+
+  patchTitle = 'Patch inconnu';
+
+  refreshIcon = faSync;
+  calendarIcon = faCalendar;
+  externalIcon = faExternalLink;
+
   isLoading = true;
+  gameError = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -21,13 +35,37 @@ export class LolGameDetailsComponent implements OnInit {
   ngOnInit(): void {
     this.gameId = this.route.snapshot.paramMap.get('id');
 
+    this.loadGame();
+  }
+
+  loadGame() {
+    this.isLoading = true;
     this.lolService.getGame(this.gameId).subscribe(
       (game) => {
         this.game = game;
+
+        if (this.game.gameVersion) {
+          this.patchTitle = `Patch ${this.game.gameVersion}`;
+        }
+
         this.isLoading = false;
         console.log(this.game);
       },
       (err) => {
+        this.gameError = true;
+        console.error(err);
+      }
+    );
+  }
+
+  updateGame(): void {
+    this.isLoading = true;
+    this.lolService.refreshGame(this.gameId).subscribe(
+      (x) => {
+        this.loadGame();
+      },
+      (err) => {
+        this.gameError = true;
         console.error(err);
       }
     );
